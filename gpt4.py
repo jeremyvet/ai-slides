0cc6f671ca2eb1f174e43d25805a1e1b6cd7e15d
Rubyboat1207 <rudy@rudysoliz.com>
2023-12-05 13:23:02 -0600

    if name is main gpt4

--- gpt4.py
+++ gpt4.py
@@ -42,102 +42,103 @@ def delegate_function_call(generator: SlideshowGenerator, name: str, arguments: 
 
 gen = SlideshowGenerator()
 
-while True:
-    RESPONSE = client.chat.completions.create(
-        model="slidesai",
-        messages=msgs,
-        tools=[
-            {
-                "type": "function",
-                "function": {
-                    "name": "create_title",
-                    "description": "Creates title slide, used best as first slide of presentation or to start a seperate section",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "theme": {
-                                "type": "string",
-                                "enum": themes,
-                                "description": "The theme of the current slide. Should be kept consistent througout the slideshow except when specifically told otherwise.",
-                            },
-                            "title": {
-                                "type": "string",
-                                "description": "Text of Title of title slide",
-                            },
-                            "subtitle": {
-                                "type": "string",
-                                "description": "Text of subtitle of title slide",
-                            },
-                        },
-                        "required": ["title", "subtitle", "theme"],
-                    },
-                }
-            },
-            {
-                "type": "function",
-                "function": {
-                    "name": "create_content_slide",
-                    "description": "Creates a content slide. Each content slide has a title and a body, the title defines the main idea of the slide and the body provides a general description of the main idea. Use this slide for explaining topics and introducing information.",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "theme": {
-                                "type": "string",
-                                "enum": themes,
-                                "description": "The theme of the current slide. Should be kept consistent througout the slideshow except when specifically told otherwise.",
-                            },
-                            "title": {
-                                "type": "string",
-                                "description": "Text of Title of title and body slide",
-                            },
-                            "content": {
-                                "type": "string",
-                                "description": "Text of body of title and body slide",
-                            },
-                        },
-                        "required": ["title", "content", "theme"],
-                    },
-                }
-            },
-            {
-                "type": "function",
-                "function": {
-                    "name": "save",
-                    "description": "Completes the slideshow, and sends it to the user.",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                        },
-                        "required": [],
-                    },
-                }
-            }
-        ]
-    )
-
-    choice = RESPONSE.choices[0]
-    if choice.message.content is None:
-        choice.message.content = ""
-    msgs.append(choice.message)
-
-    if choice.finish_reason == 'tool_calls':
-        for call in choice.message.tool_calls:
-            function_name = call.function.name
-            print(call)
-
-            content = delegate_function_call(gen, call.function.name, call.function.arguments)
-
-            print(content)
-            msgs.append(
+if __name__ == "__main__":
+    while True:
+        RESPONSE = client.chat.completions.create(
+            model="slidesai",
+            messages=msgs,
+            tools=[
                 {
-                    "tool_call_id": call.id,
-                    "role": "tool",
-                    "name": function_name,
-                    "content": content,
+                    "type": "function",
+                    "function": {
+                        "name": "create_title",
+                        "description": "Creates title slide, used best as first slide of presentation or to start a seperate section",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "theme": {
+                                    "type": "string",
+                                    "enum": themes,
+                                    "description": "The theme of the current slide. Should be kept consistent througout the slideshow except when specifically told otherwise.",
+                                },
+                                "title": {
+                                    "type": "string",
+                                    "description": "Text of Title of title slide",
+                                },
+                                "subtitle": {
+                                    "type": "string",
+                                    "description": "Text of subtitle of title slide",
+                                },
+                            },
+                            "required": ["title", "subtitle", "theme"],
+                        },
+                    }
+                },
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "create_content_slide",
+                        "description": "Creates a content slide. Each content slide has a title and a body, the title defines the main idea of the slide and the body provides a general description of the main idea. Use this slide for explaining topics and introducing information.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "theme": {
+                                    "type": "string",
+                                    "enum": themes,
+                                    "description": "The theme of the current slide. Should be kept consistent througout the slideshow except when specifically told otherwise.",
+                                },
+                                "title": {
+                                    "type": "string",
+                                    "description": "Text of Title of title and body slide",
+                                },
+                                "content": {
+                                    "type": "string",
+                                    "description": "Text of body of title and body slide",
+                                },
+                            },
+                            "required": ["title", "content", "theme"],
+                        },
+                    }
+                },
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "save",
+                        "description": "Completes the slideshow, and sends it to the user.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                            },
+                            "required": [],
+                        },
+                    }
                 }
-            )
-    elif choice.finish_reason == 'stop':
-        print(choice.message.content)
-        msgs.append({"role": "user", "content": input()})
+            ]
+        )
+
+        choice = RESPONSE.choices[0]
+        if choice.message.content is None:
+            choice.message.content = ""
+        msgs.append(choice.message)
+
+        if choice.finish_reason == 'tool_calls':
+            for call in choice.message.tool_calls:
+                function_name = call.function.name
+                print(call)
+
+                content = delegate_function_call(gen, call.function.name, call.function.arguments)
+
+                print(content)
+                msgs.append(
+                    {
+                        "tool_call_id": call.id,
+                        "role": "tool",
+                        "name": function_name,
+                        "content": content,
+                    }
+                )
+        elif choice.finish_reason == 'stop':
+            print(choice.message.content)
+            msgs.append({"role": "user", "content": input()})
 
 
